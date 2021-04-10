779d3a9ec1f0c9d1a4c33edc20026cbeb243a227
Jon Craton <jncraton@gmail.com>
2021-04-10 13:37:01 -0400

    Add strokeLine method

--- ctx2dext.js
+++ ctx2dext.js
@@ -15,3 +15,10 @@ CanvasRenderingContext2D.prototype.fillCircle = function (x, y, radius) {
 CanvasRenderingContext2D.prototype.point = function (x, y, size=2) {
   this.fillRect(x - size/2, y-size/2, size, size)
 }
+
+CanvasRenderingContext2D.prototype.strokeLine = function (x1, y1, x2, y2) {
+  this.beginPath()
+  this.moveTo(x1, y1)
+  this.lineTo(x2, y2)
+  this.stroke()
+}

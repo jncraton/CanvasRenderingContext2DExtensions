9ce6b9b33a54445989df803d8f9be47b55f8ac57
Jon Craton <jncraton@gmail.com>
2021-04-10 14:21:56 -0400

    Add strokeLabelledRect

--- ctx2dext.js
+++ ctx2dext.js
@@ -52,3 +52,15 @@ CanvasRenderingContext2D.prototype.strokeLabelledCircle = function (x, y, radius
 
   this.restore()
 }
+
+CanvasRenderingContext2D.prototype.strokeLabelledRect = function (x, y, width, height, label) {
+  this.strokeRect(x, y, width, height)
+
+  this.save()
+
+  this.textAlign = 'center'
+  this.textBaseline = 'middle'
+  this.strokeText(label, x + width/2, y + height/2)
+
+  this.restore()
+}
